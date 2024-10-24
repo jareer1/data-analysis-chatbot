3d3a18d39c8454a01cef453cb136559420239235
jareer1 <104534600+jareer1@users.noreply.github.com>
2024-10-24 19:56:53 +0500

    Update chat_app.py

--- src/chat_app.py
+++ src/chat_app.py
@@ -10,7 +10,7 @@ from agent import create_agent_for_python, create_agent_for_sql
 warnings.filterwarnings("ignore")
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-
+os.environ['OPENAI_API_KEY'] ="enter open AI api key here"
 parent_dir = os.path.join(current_dir, "..")
 
 sys.path.insert(0, parent_dir)
